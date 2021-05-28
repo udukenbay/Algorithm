c0578752c7410be24d0ec6cdd05038a5099fcfcf
Urziya Dukenbay <62696463+udukenbay@users.noreply.github.com>
2021-05-28 09:24:05 +0600

    Add files via upload

---
+++ Part1_Coursera/Lecture1/HelloWorld.java
@@ -0,0 +1,19 @@
+
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *
+ ******************************************************************************/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello, World");
+    }
+}
