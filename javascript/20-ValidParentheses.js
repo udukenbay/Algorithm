b347f864360fb50fc6814a732015cd990acfab59
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-04-12 17:04:54 +0600

    20-valid parentheses code

--- javascript/20-ValidParentheses.js
+++ javascript/20-ValidParentheses.js
@@ -4,38 +4,41 @@
  * @return {boolean}
  */
  var isValid = function(s) {
-    let pnt_1 = ['(', '[', '{'];
-    let pnt_2 = [')', ']', '}'];
-
     let str = s.split("");
     let stack = [];
-
-    let index = 0;
-    // console.log(str);
     
     for(let i=0; i < str.length; i++) {
-        //open parentheses
-        if(pnt_1.filter(el => el == str[i]) == str[i]) {
-            stack.push(str[i]);
-            // console.log("stack open:", stack);
+        let x = str[i];
+        
+        if(x == '(' || x == '[' || x == '{') {
+            stack.push(x);
+            continue;
         }
-        //closing parentheses
-        else if(pnt_2.filter(el => el == str[i]) == str[i]){
-            // console.log(str[i]);
-
-            index = pnt_2.indexOf(str[i]);
-            // console.log("index:", pnt_1[index]);
-
-            if(stack.includes(pnt_1[index])) {
-                stack.splice(stack.indexOf(pnt_1[index]), 1);
-                // console.log("stack close", stack);
-            }
+        
+        if(stack.length == 0)
+            return false;
+        
+        let check;
+        switch(x) {
+            case ')':
+                check = stack.pop();
+                if(check == '[' || check == '{')
+                    return false;
+                break;
+            case ']':
+                check = stack.pop();
+                if(check == '(' || check == '{')
+                    return false;
+                break;
+            case '}':
+                check = stack.pop();
+                if(check == '(' || check == '[')
+                    return false;
+                break;
         }
     }
-
-    if(stack.length > 0) {
-        return false;
-    } else return true;
+    
+    return (stack.length == 0);
 };
 
 console.log(isValid("()"));//true
@@ -44,6 +47,7 @@ console.log(isValid("(]"));//false
 console.log(isValid("(){}}{"));//false
 console.log(isValid("({{{{}}}))"));//false
 console.log(isValid("{[]}"));//true
+console.log(isValid("([)]"));//false
 
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
