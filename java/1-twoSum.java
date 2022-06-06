caee7c213181d33d5196c6a7e1c589e700b62b56
u.dukenbay <urziya.dukenbay@gmail.com>
2022-06-06 20:29:56 +0600

    twoSum in java

---
+++ java/1-twoSum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Map<Integer, Integer> map = new HashMap();
+
+        int[] res = new int[2];
+        // for(int i = 0; i < nums.length; i++) {
+        //     int n = nums[i];
+        //     if(map.containsKey(target - n)) {
+        //         res[0] = i;
+        //         res[1] = map.get(target - n);
+        //         break;
+        //     }
+        //     map.put(n, i);
+        // }
+
+        return res;
+    }
+}
