85490f588b252bdc984caaecaf14f4e2711eee2e
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-06-28 17:34:52 +0600

    neetcode tasks

--- javascript/217-containsDuplicate.js
+++ javascript/217-containsDuplicate.js
@@ -20,9 +20,8 @@
     let set = [];
 
     for(let i = 0; i < nums.length; i++) {
-
         if(set.includes(nums[i])) return true;
-            set.push(nums[i]);
+        set.push(nums[i]);
     }
     return false;
 };
@@ -30,3 +29,18 @@
 console.log(containsDuplicate([1,2,3,1]));
 console.log(containsDuplicate([1,2,3,4]));
 // console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
+
+/**ACCEPTED 28.06.2022
+ */
+//  var containsDuplicate = function(nums) {
+//     let result = false;
+//     for(let i = 0; i < nums.length; i++) {
+//         for(let j = i+1; j < nums.length; j++) {
+//             if(nums[i] == nums[j]) {
+//                 result = true;
+//                 break;
+//             }
+//         }
+//     }
+//     return result;
+// };
--- javascript/242-isAnagram.js
+++ javascript/242-isAnagram.js
@@ -4,8 +4,36 @@
  * @return {boolean}
  */
 // https://leetcode.com/problems/valid-anagram/
- var isAnagram = function(s, t) {
+//  var isAnagram = function(s, t) {
+//     if(s.length != t.length)
+//         return false;
     
+//     let sHash = {};
+//     let tHash = {};
+
+//     for(let i = 0; i < s.length; i++) {
+//         if(!sHash[s[i]]) sHash[s[i]] = 1;
+//         else sHash[s[i]] += 1;
+
+//         if(!tHash[t[i]]) tHash[t[i]] = 1;
+//         else tHash[t[i]] += 1;
+//     }
+
+//     for (const [key, value] of Object.entries(sHash)) {
+//         if(sHash[key] != tHash[key])
+//             return false;
+//     }
+
+//     return true;
+// };
+
+// 2 - SOLUTION
+var isAnagram = function(s, t) {
+    s.sort((a, b) => (a.attr > b.attr) - (a.attr < b.attr));
+    t.sort((a, b) => (a.attr > b.attr) - (a.attr < b.attr));
+    
+    console.log(s);
+    console.log(t);
 };
 
 console.log(isAnagram("anagram", "nagaram"));
