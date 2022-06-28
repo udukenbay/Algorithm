6ea527a4804a88c9b9b9866eaf4871bb3b118bc8
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-06-28 14:08:09 +0600

    hash lecture pseudo code

--- java/1-twoSum.java
+++ java/1-twoSum.java
@@ -1,17 +1,19 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Map<Integer, Integer> map = new HashMap();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         int[] res = new int[2];
-        // for(int i = 0; i < nums.length; i++) {
-        //     int n = nums[i];
-        //     if(map.containsKey(target - n)) {
-        //         res[0] = i;
-        //         res[1] = map.get(target - n);
-        //         break;
-        //     }
-        //     map.put(n, i);
-        // }
+        for(int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            if(map.containsKey(target - n)) {
+                res[0] = i;
+                res[1] = map.get(target - n);
+                break;
+            }
+            map.put(n, i);
+        }
 
         return res;
     }
