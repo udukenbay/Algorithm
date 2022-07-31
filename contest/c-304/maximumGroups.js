fc5087126253fdecbff2e5bc2967e19ff89da92e
u.dukenbay <urziya.dukenbay@gmail.com>
2022-07-31 10:30:18 +0600

    maximumGroups

---
+++ contest/c-304/maximumGroups.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} grades
+ * @return {number}
+ * https://leetcode.com/contest/weekly-contest-304/problems/maximum-number-of-groups-entering-a-competition/
+ */
+ var maximumGroups = function(grades) {
+    
+};
