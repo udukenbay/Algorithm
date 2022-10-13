dc202f7a2432a2642d4f8f757c25ef3d9f98988b
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-10-13 08:45:30 +0600

    maxWealth

--- javascript/1672-maximumWealth.js
+++ javascript/1672-maximumWealth.js
@@ -14,3 +14,24 @@
 
   return Math.max(...wealth)
 };
+
+//*** 2 solution */
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+ var maximumWealth = function(accounts) {
+  let maxWealth = 0;
+
+  for(let i=0; i<accounts.length; i++) {
+      let currentWealth = 0;
+      for(let j=0; j<accounts[i].length; j++) {
+          currentWealth += accounts[i][j];
+      }
+
+      if(currentWealth > maxWealth)
+          maxWealth = currentWealth;
+  }
+
+  return maxWealth
+};
