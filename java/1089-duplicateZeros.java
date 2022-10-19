f1b099d973342191cd382ebe1fdd588bdf521c7c
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-10-19 17:10:48 +0600

    1089. Duplicate Zeros

---
+++ java/1089-duplicateZeros.java
@@ -0,0 +1,17 @@
+class Solution {
+  public void duplicateZeros(int[] arr) {
+      for(int i=0; i<arr.length; i++) {
+          if(arr[i] == 0 && i+1 != arr.length) {
+              for(int j = arr.length-1; j > i; j--) {
+                  arr[j] = arr[j-1];
+              }
+
+              arr[i+1] = 0;
+              i++;
+
+          }
+      }
+
+      System.out.println(arr);
+  }
+}
