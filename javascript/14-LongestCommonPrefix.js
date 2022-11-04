44e4ee6bf516143fb7881fe1d09995682ced89e5
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-11-04 14:40:03 +0600

    my decision 14. Longest Common Prefix

--- javascript/14-LongestCommonPrefix.js
+++ javascript/14-LongestCommonPrefix.js
@@ -5,31 +5,81 @@
  * @param {string[]} strs
  * @return {string}
  */
- var longestCommonPrefix = function(strs) {
-    let size = strs.length;
-    let pref = "";
+//  var longestCommonPrefix = function(strs) {
+//     let size = strs.length;
+//     let pref = "";
 
-    if(size == 0)
-        return "";
-    
-    if(size == 1)
-        return strs[0];
-    
-    strs.sort();
+//     if(size == 0)
+//         return "";
 
-    let minSize = Math.min(strs[0].length, strs[size-1].length);
+//     if(size == 1)
+//         return strs[0];
 
-    let i = 0;
-    while(i < minSize && strs[0][i] == strs[size-1][i]) {
-        i++;
-        pref = strs[0].substring(0, i);
-    }
+//     strs.sort();
 
-    return pref;
-};
+//     let minSize = Math.min(strs[0].length, strs[size-1].length);
+
+//     let i = 0;
+//     while(i < minSize && strs[0][i] == strs[size-1][i]) {
+//         i++;
+//         pref = strs[0].substring(0, i);
+//     }
+
+//     return pref;
+// };
 
 console.log(longestCommonPrefix(["flower","flow","flight"]));
 console.log(longestCommonPrefix(["dog","racecar","car"]));
 console.log(longestCommonPrefix(["geeksforgeeks", "geeks", "geek", "geezer"]));
 console.log(longestCommonPrefix(["apple", "ape", "april"]));
 console.log(longestCommonPrefix(["aaa","aa","aaa"]));
+
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+    // sort
+    for(let i=0; i < strs.length; i++) {
+        for(let j=i; j < strs.length-1; j++) {
+            if(strs[i].length > strs[j].length) {
+                let temp = strs[j];
+                strs[j]=strs[i];
+                strs[i]=temp;
+            }
+        }
+    }
+
+    console.log(strs);
+
+    let box = [];
+    let word = strs[0];
+
+    for(let i=0; i< word.length; i++) {
+
+        let check = true;
+
+        for(let j=0; j<strs.length; j++) {
+            let item = strs[j];
+
+            console.log(item);
+            console.log('item'+i,item.substring(0, i+1));
+            console.log('word'+i, word.substring(0, i+1));
+
+            if(item.substring(0, i+1) != word.substring(0, i+1)) {
+                check = false;
+            }
+        }
+
+        if(check) {
+            box.push(word.substring(0, i+1));
+        }
+    }
+
+    console.log(box);
+
+    if(box.length > 0)
+        return box[box.length-1];
+    else return "";
+};
