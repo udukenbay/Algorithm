2d5d02bd2024ffdd2925980adf7521f8a4a85f0d
u.dukenbay <urziya.dukenbay@gmail.com>
2023-01-17 23:48:15 +0600

    643 yet not completed

---
+++ javascript/643-findMaxAverage.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+  let avg = [];
+  let i = 0;
+
+  while(i < nums.length) {
+      let j = 0;
+      let sum = 0;
+      while(j < k && i < nums.length) {
+        sum += nums[i+j];
+        j++;
+      }
+
+      avg.push(sum/k);
+      i++;
+  }
+
+  avg = avg.filter(Boolean);
+
+  return Math.max(...avg);
+};
+
+console.log(findMaxAverage([1,12,-5,-6,50,3], 4))
