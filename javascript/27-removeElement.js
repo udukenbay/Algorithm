07ea7d08bedcc19d3e78753dbaf4d9a9dc6fcc3c
Дукенбай Урзия Әбдіхалыққызы <urziya.dukenbay@kaspi.kz>
2023-01-20 17:34:08 +0600

    27-removeElement.js in-place solution

--- javascript/27-removeElement.js
+++ javascript/27-removeElement.js
@@ -21,3 +21,20 @@
 };
 
 console.log(removeElement([3,2,2,3], 3));
+
+// in-Place
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0;
+    for(let i=0; i < nums.length; i++) {
+        if(nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
